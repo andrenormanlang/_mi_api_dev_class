cfc610861517eec6010a0304851744c85694402c
andrenormanlang <andrenormanlang@gmail.com>
2023-02-22 21:26:25 +0100

    commited finished file

--- 25-Chat/backend/src/types/shared/SocketTypes.ts
+++ 25-Chat/backend/src/types/shared/SocketTypes.ts
@@ -3,12 +3,12 @@ export {}
 // Events emitted by the server to the client
 export interface ServerToClientEvents {
 	hello: () => void
-	newChatMessage: (data: ChatMessageData) => void
+	chatMessage: (message: ChatMessageData) => void
 }
 
 // Events emitted by the client to the server
 export interface ClientToServerEvents {
-	sendChatMessage: (data: ChatMessageData) => void
+	sendChatMessage: (message: ChatMessageData) => void
 }
 
 // Events between servers
@@ -17,7 +17,7 @@ export interface InterServerEvents {
 
 // Message payload
 export interface ChatMessageData {
-	content: string
-	// timestamp: number
-	// username: string
+	content:string
+	// timestamp:number
+	// username:string
 }
